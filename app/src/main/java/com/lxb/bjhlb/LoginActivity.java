cb75ce59b01c8372bfe867b8b516ebeaff95c8fa
lxb <424151385@qq.com>
2017-09-19 12:13:38 +0800

    =======

--- app/src/main/java/com/lxb/bjhlb/LoginActivity.java
+++ app/src/main/java/com/lxb/bjhlb/LoginActivity.java
@@ -335,7 +335,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent(LoginActivity.this, TestMainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MyMainActivity.class);
                 startActivity(intent);
                 finish();
             } else {
---
+++ app/src/main/java/com/lxb/bjhlb/MyMainActivity.java
@@ -0,0 +1,179 @@
+package com.lxb.bjhlb;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnNeverAskAgain;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MyMainActivity extends AppCompatActivity {
+
+    private WebView loginWv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_main);
+
+        loginWv = (WebView) findViewById(R.id.login_wv);
+
+        WebSettings webSettings = loginWv.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        // 设置可以访问文件
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setUseWideViewPort(true);//将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true);// 缩放至屏幕的大小
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);//适应内容大小
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);//适应屏幕，内容将自动缩放
+        //设置编码格式
+        webSettings.setDefaultTextEncodingName("utf-8");
+        // 启用地位
+        webSettings.setGeolocationEnabled(true);
+
+        webSettings.setSupportZoom(true);
+        webSettings.setTextZoom(350);
+
+        // 根据网络是否可用，动态设置缓存策略
+//        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//        if (networkInfo.isAvailable()) {
+//            webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+//        } else {
+//            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ONLY);
+//        }
+
+        loginWv.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, true);
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+            }
+        });
+
+        loginWv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Log.i("====>",request.toString());
+                view.loadUrl(request.toString());
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+        });
+//        loginWv.loadUrl("http://192.168.1.115:8080/sdrpoms");
+        loginWv.loadUrl("file:///android_asset/location.html");
+
+        MyMainActivityPermissionsDispatcher.needsPermissionWithCheck(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (loginWv != null) {
+            loginWv.saveState(outState);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loginWv.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        loginWv.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (loginWv != null) {
+            ViewGroup parent = (ViewGroup) loginWv.getParent();
+            if (parent != null) {
+                parent.removeView(loginWv);
+            }
+            loginWv.removeAllViews();
+            loginWv.destroy();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //如果按下的是回退键且历史记录里确实还有页面
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && loginWv.canGoBack()) {
+            loginWv.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void needsPermission() {
+        Toast.makeText(this, "定位权限已经获取", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MyMainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void onShowRationale(final PermissionRequest request) {
+        new AlertDialog.Builder(this)
+                .setMessage("需要申请定位权限")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 再次执行请求
+                        request.proceed();
+                    }
+                })
+                .show();
+    }
+
+    @OnPermissionDenied({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void onPermissionDenied() {
+        Toast.makeText(this, "权限被拒绝", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnNeverAskAgain({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void onNeverAskAgain() {
+        Toast.makeText(this, "不再询问", Toast.LENGTH_SHORT).show();
+    }
+}
